06704d0122e14346c3472b4c2a0fcd5afb970066
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-08-26 09:56:22 +0900

    fix: Teacher Data READ API 수정. 회원가입 시 Database_Info 정보를 받아오지 못하는 버그로 인해 Select Query 방식 수정

--- controller/teacher.js
+++ controller/teacher.js
@@ -91,14 +91,18 @@ const teacherController = {
     console.log("KK Teacher Data READ API 호출");
     try {
       const query = req.query;
-      const { classIdx, dayofweek } = query;
+      const { classIdx, dayofweek } = query; // classIdx 필수, dayofweek 선택
       const teacher_table = KK_User_Table_Info["teacher"].table;
-      const teacher_attribute = KK_User_Table_Info["teacher"].attribute;
       const teacher_class_table = KK_User_Table_Info["teacher_class"].table;
-      const teacher_class_attribute =
-        KK_User_Table_Info["teacher_class"].attribute;
+
+      // 회원가입 시 KK_User_Table_Info 데이터를 로드하지 못하는 버그로 인해 사용 불가
+      // const teacher_attribute = KK_User_Table_Info["teacher"].attribute;
+      // const teacher_class_attribute =
+      //   KK_User_Table_Info["teacher_class"].attribute;
 
       // teacher_class 테이블 Join Select
+      // kk_teacher 테이블과 kk_teacher_class 테이블을 kk_teacher_idx 속성으로 Join
+      // 이후 query 조건을 통해
       const select_query = `SELECT t.kk_teacher_idx, t.kk_teacher_introduction, t.kk_teacher_name FROM ${teacher_table} AS t JOIN ${teacher_class_table} AS tc ON t.kk_teacher_idx = tc.kk_teacher_idx WHERE tc.kk_class_idx = ${classIdx}${` AND t.kk_teacher_approve_status = '1'`}${
         dayofweek ? ` AND t.kk_teacher_dayofweek LIKE '%${dayofweek}%'` : ""
       } ORDER BY t.kk_teacher_created_at DESC;`;
