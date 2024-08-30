670afb61f293e3a3effa8dd3cd8d2c80a2bf09dc
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-08-30 16:33:41 +0900

    feat: 마이페이지 강사 출석 API 수정. 예약 기관명 데이터 추가 반환

--- controller/mypage.js
+++ controller/mypage.js
@@ -36,11 +36,11 @@ const mypageController = {
   getKKTeacherAttendDataRead: async (req, res) => {
     console.log("KK Teacher Attend Data READ API 호출");
     try {
-      const { pageNum, userClass, name, userIdx } = req.query;
+      const { pageNum, userIdx } = req.query;
       // 클라이언트로부터 페이지 번호 받기 (기본값: 1)
       // console.log(pageNum);
       const page = pageNum || 1;
-      const limit = 5; // 한 페이지에 보여줄 리뷰의 수
+      const limit = 10; // 한 페이지에 보여줄 리뷰의 수
       const offset = (page - 1) * limit;
 
       if (true) {
@@ -52,7 +52,7 @@ JOIN kk_reservation AS r ON a.kk_reservation_idx = r.kk_reservation_idx
 WHERE r.kk_teacher_idx = '${userIdx}' AND r.kk_reservation_approve_status = '1'`;
         const count_data = await fetchUserData(connection_KK, count_query);
         const lastPageNum = Math.ceil(count_data[0]["COUNT(*)"] / limit);
-        console.log(lastPageNum);
+        // console.log(lastPageNum);
 
         // SQL 쿼리 준비: 최신순으로 유저 데이터 가져오기
         // const select_query = `SELECT * FROM ${user_table} WHERE kk_${userClass}_approve_status = '0' ORDER BY kk_${userClass}_created_at DESC LIMIT ? OFFSET ?`;
@@ -61,14 +61,16 @@ WHERE r.kk_teacher_idx = '${userIdx}' AND r.kk_reservation_approve_status = '1'`
     t.kk_teacher_name,
     a.kk_attend_idx,
     a.kk_attend_date,
-    a.kk_attend_status
+    a.kk_attend_status,
+    ag.kk_agency_name
 FROM kk_attend AS a
 JOIN kk_reservation AS r ON a.kk_reservation_idx = r.kk_reservation_idx
 JOIN kk_teacher AS t ON r.kk_teacher_idx = t.kk_teacher_idx
 JOIN kk_class AS c ON r.kk_class_idx = c.kk_class_idx
+JOIN kk_agency AS ag ON r.kk_agency_idx = ag.kk_agency_idx
 WHERE r.kk_teacher_idx = '${userIdx}'
 AND r.kk_reservation_approve_status = '1'
-ORDER BY a.kk_attend_created_at DESC LIMIT ? OFFSET ?;
+ORDER BY a.kk_attend_date DESC LIMIT ? OFFSET ?;
 `;
         const select_values = [limit, offset];
         // 데이터베이스 쿼리 실행
@@ -98,7 +100,7 @@ ORDER BY a.kk_attend_created_at DESC LIMIT ? OFFSET ?;
       res.status(500).json({ message: "Server Error - 500" });
     }
   },
-  // TODO# KK Teacher Attend Data Update
+  // KK Teacher Attend Data Update
   postKKTeacherAttendDataUpdate: (req, res) => {
     console.log("Teacher Attend Data Update API 호출");
     const { AttendData } = req.body;
@@ -140,41 +142,68 @@ ORDER BY a.kk_attend_created_at DESC LIMIT ? OFFSET ?;
     }
   },
   // TODO# KK Agency Reservation Data READ
-  getKKAgencyReservationDataRead: (req, res) => {
-    console.log("KK Teacher Data READ API 호출");
+  getKKAgencyReservationDataRead: async (req, res) => {
+    console.log("KK Agency Reservation Data READ API 호출");
     try {
-      const query = req.query;
-      const { classIdx, dayofweek } = query; // classIdx 필수, dayofweek 선택
-      const teacher_table = KK_User_Table_Info["teacher"].table;
-      const teacher_class_table = KK_User_Table_Info["teacher_class"].table;
+      const { pageNum, userIdx } = req.query;
+      // 클라이언트로부터 페이지 번호 받기 (기본값: 1)
+      // console.log(pageNum);
+      const page = pageNum || 1;
+      const limit = 10; // 한 페이지에 보여줄 리뷰의 수
+      const offset = (page - 1) * limit;
 
-      // 회원가입 시 KK_User_Table_Info 데이터를 로드하지 못하는 버그로 인해 사용 불가
-      // const teacher_attribute = KK_User_Table_Info["teacher"].attribute;
-      // const teacher_class_attribute =
-      //   KK_User_Table_Info["teacher_class"].attribute;
+      if (true) {
+        // Pagination Last Number Select
+        // const count_query = `SELECT COUNT(*) FROM kk_reservation WHERE kk_teacher_idx = '${userIdx}'`;
+        const count_query = `SELECT COUNT(*)
+FROM kk_attend AS a
+JOIN kk_reservation AS r ON a.kk_reservation_idx = r.kk_reservation_idx
+WHERE r.kk_teacher_idx = '${userIdx}' AND r.kk_reservation_approve_status = '1'`;
+        const count_data = await fetchUserData(connection_KK, count_query);
+        const lastPageNum = Math.ceil(count_data[0]["COUNT(*)"] / limit);
+        // console.log(lastPageNum);
 
-      // teacher_class 테이블 Join Select
-      // kk_teacher 테이블과 kk_teacher_class 테이블을 kk_teacher_idx 속성으로 Join
-      // 이후 query 조건을 통해
-      const select_query = `SELECT t.kk_teacher_idx, t.kk_teacher_introduction, t.kk_teacher_name FROM ${teacher_table} AS t JOIN ${teacher_class_table} AS tc ON t.kk_teacher_idx = tc.kk_teacher_idx WHERE tc.kk_class_idx = ${classIdx}${` AND t.kk_teacher_approve_status = '1'`}${
-        dayofweek ? ` AND t.kk_teacher_dayofweek LIKE '%${dayofweek}%'` : ""
-      } ORDER BY t.kk_teacher_created_at DESC;`;
-
-      // console.log(select_query);
-      // 데이터베이스 쿼리 실행
-      connection_KK.query(select_query, null, (err, data) => {
-        if (err) {
-          console.log(err);
-          return res.status(404).json({
-            message: err.sqlMessage,
+        // SQL 쿼리 준비: 최신순으로 유저 데이터 가져오기
+        // const select_query = `SELECT * FROM ${user_table} WHERE kk_${userClass}_approve_status = '0' ORDER BY kk_${userClass}_created_at DESC LIMIT ? OFFSET ?`;
+        const select_query = `SELECT
+    c.kk_class_title,
+    t.kk_teacher_name,
+    a.kk_attend_idx,
+    a.kk_attend_date,
+    a.kk_attend_status,
+    ag.kk_agency_name
+FROM kk_attend AS a
+JOIN kk_reservation AS r ON a.kk_reservation_idx = r.kk_reservation_idx
+JOIN kk_teacher AS t ON r.kk_teacher_idx = t.kk_teacher_idx
+JOIN kk_class AS c ON r.kk_class_idx = c.kk_class_idx
+JOIN kk_agency AS ag ON r.kk_agency_idx = ag.kk_agency_idx
+WHERE r.kk_teacher_idx = '${userIdx}'
+AND r.kk_reservation_approve_status = '1'
+ORDER BY a.kk_attend_date DESC LIMIT ? OFFSET ?;
+`;
+        const select_values = [limit, offset];
+        // 데이터베이스 쿼리 실행
+        connection_KK.query(select_query, select_values, (err, data) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({
+              message: "User SignUp Request READ Fail! - 400",
+              page: -1,
+              limit: -1,
+              lastPageNum: -1,
+              data: [],
+            });
+          }
+          // 결과 반환
+          return res.status(200).json({
+            message: "User SignUp Request READ Success! - 200",
+            page,
+            limit,
+            lastPageNum,
+            data: data,
           });
-        }
-        // 결과 반환
-        return res.status(200).json({
-          message: "Teacher Access Success! - 200 OK",
-          data,
         });
-      });
+      }
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "Server Error - 500" });
