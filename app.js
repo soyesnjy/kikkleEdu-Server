7e76d20235f627aa7ff08f3d209cb77bd46c90dc
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-09-04 16:45:12 +0900

    feat: req.body 제한 100mb 변경

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(
 );
 
 // BodyParser 추가. post, put 요청의 req.body 구문 해석 기능 제공.
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // cookie-parser 추가.
 const cookieParser = require("cookie-parser");
